9b6abbfc64a839a7afea4872717f4eb7f52efd56
unknown <lovelyosg16@gmail.com>
2023-05-23 16:38:29 +0900

    baekstudy0523

---
+++ baekstudy/src/Baek1008.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Baek1008 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double A = sc.nextInt();
+        double B = sc.nextInt();
+        sc.close();
+
+        if(0<A & B<10) System.out.println(A/B);
+
+    }
+}
